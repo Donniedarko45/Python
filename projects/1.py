7278951d15a5f0d84fa9ba9839f5f1d5c741359a
kirti pandey <kirti.azby@gmail.com>
2024-06-03 10:29:14 +0530

    hello

--- projects/1.py
+++
@@ -1,6 +0,0 @@
-try:
-    # Code block where exceptions may occur
-    result = 10 / 0  # This will raise a ZeroDivisionError
-except ZeroDivisionError:
-    # Code block to handle the specific exception
-    print("Error: Division by zero occurred!")
--- temp.py
+++
@@ -1,9 +0,0 @@
-import random
-
-import pyautogui as pg
-
-import time
-
-time.sleep(5)
-pg.write(" life")
-pg.press('enter')
